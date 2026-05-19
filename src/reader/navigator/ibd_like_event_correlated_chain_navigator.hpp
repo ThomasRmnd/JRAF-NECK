aa6824fc21197f8c93d52de698feab215179c007
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2026-05-19 21:32:00 +0200

    Try

--- src/reader/navigator/ibd_like_event_correlated_chain_navigator.hpp
+++ src/reader/navigator/ibd_like_event_correlated_chain_navigator.hpp
@@ -87,8 +87,8 @@ public:
         if (!navigator_base::entry(n)) return false;
 
         const timestamp ts = prompt.ts;
-        const timestamp lo = ts + timestamp{0, -1000};
-        const timestamp hi = ts + timestamp{0,  1000};
+        const timestamp lo = ts + timestamp{-8, 0};
+        const timestamp hi = ts + timestamp{8, 0};
 
         neutrons.clear();
         corrlator_results res = m_neu_corr->correlate(lo, hi);

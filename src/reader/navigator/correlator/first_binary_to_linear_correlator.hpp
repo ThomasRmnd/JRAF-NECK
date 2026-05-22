4bc6eaa6d52fd2c54101297c72068bdc162ade22
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2026-05-22 18:40:44 +0200

    Debug job error

--- src/reader/navigator/correlator/first_binary_to_linear_correlator.hpp
+++ src/reader/navigator/correlator/first_binary_to_linear_correlator.hpp
@@ -21,7 +21,7 @@ public:
             m_last_idx = res.lower;
             std::cout << "[Debug]: Get last index\n";
             m_nav->entry(m_last_idx);
-            std::cout << "[Debug]: Get last timestamp (m_nav = " << m_nav << ")\n";
+            std::cout << "[Debug]: Get last timestamp (m_nav = " << m_nav << ", m_last_idx = " << m_last_idx << ")\n";
             m_last_ts = m_nav->ts();
             std::cout << "[Debug]: Returning\n";
             return res;

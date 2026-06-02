4500403e6e90cd7329ef91365b81006bb7cdf4d9
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2026-06-03 00:09:49 +0200

    Corrected chimney cut

--- src/analysis/ibd/ibd_analysis.hpp
+++ src/analysis/ibd/ibd_analysis.hpp
@@ -38,13 +38,13 @@ public:
         analysis_base{name}
     {
         std::string treename = "IBDAnalysis" + suffix;
-        m_nav = navigator_manager::retrieve<ibd_like_event_correlated_chain_navigator>(
-            filepath, treename, 
-            filepath, "NeutronAnalysis" + suffix, 
-            filepath, "MultiplicityAnalysis" + suffix, 
-            reconstruction_filepath, "muons"
-        );
-        // m_nav = navigator_manager::retrieve<ibd_like_event_navigator>(filepath, treename);
+        // m_nav = navigator_manager::retrieve<ibd_like_event_correlated_chain_navigator>(
+        //     filepath, treename, 
+        //     filepath, "NeutronAnalysis" + suffix, 
+        //     filepath, "MultiplicityAnalysis" + suffix, 
+        //     reconstruction_filepath, "muons"
+        // );
+        m_nav = navigator_manager::retrieve<ibd_like_event_navigator>(filepath, treename);
         if (!m_nav->is_valid()) {
             std::cerr << "Cannot retrieve navigator of filepath " << filepath << " and treename " << treename << '\n';
             return;
@@ -73,7 +73,8 @@ public:
 
 protected:
 
-    std::shared_ptr<ibd_like_event_correlated_chain_navigator> m_nav;
+    // std::shared_ptr<ibd_like_event_correlated_chain_navigator> m_nav;
+    std::shared_ptr<ibd_like_event_navigator> m_nav;
 
     global_scale_factor_corrector m_gtc;
 

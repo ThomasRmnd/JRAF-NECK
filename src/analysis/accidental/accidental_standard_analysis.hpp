ba7a1a2c96b82735ee00559a8a16d95a0b4052d4
Thomas Raymond <thomas.raymond@iphc.cnrs.fr>
2026-06-23 11:07:15 +0200

    Extend fiducial volume to 17.2 m

--- src/analysis/accidental/accidental_standard_analysis.hpp
+++ src/analysis/accidental/accidental_standard_analysis.hpp
@@ -64,7 +64,7 @@ protected:
 
     const energy_range_selection c_prompt_energy_cut{0.7, 12.0};
     const energy_range_selection c_delayed_energy_cut{2.0, 2.5};
-    const fiducial_volume_selection c_fiducial_volume_cut{16500.0};
+    const fiducial_volume_selection c_fiducial_volume_cut{17200.0};
     const chimney_selection c_chimney_cut{15500.0, 2000.0};
     const flasher_selection c_flasher_cut{0.55, 0.45, 170.0, 80.0};
 
